0fbdc18cb6704931b57ff060cfceb178e44bb6b6
unknown <dvd_gatik@hotmail.com>
2023-07-20 17:32:00 -0600

    Usando Base de datos en Memoria

--- projectef/Program.cs
+++ projectef/Program.cs
@@ -1,12 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using projectef.Models;
+using Microsoft.AspNetCore.Mvc;
 
-//Archivo de Clases
-// Archivo de Projectos projectref
-//appsettings archivos de propiedaddes de configuracion para la ejecucion
-//
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
 var app = builder.Build();
 
 // Rutas
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => 
+{
+    dbContext.Database.EnsureCreated();
+    return  Results.Ok("Base de datos en Memoria: " + dbContext.Database.IsInMemory());
+});
+
 app.Run();
+
+// dotnet build
+// dotnet run
