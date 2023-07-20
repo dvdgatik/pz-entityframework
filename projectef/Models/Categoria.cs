122d7602548c3e79f03f52f40af617dc26398a68
unknown <dvd_gatik@hotmail.com>
2023-07-20 17:13:00 -0600

    Data Anotations

--- projectef/Models/Categoria.cs
+++ projectef/Models/Categoria.cs
@@ -1,11 +1,17 @@
 namespace projectef.Models;
+using System.ComponentModel.DataAnnotations;
 
 //Creacion de modelos que se convertiran en tablas de la base de datos
 
 //Clase Categoria y propiedades
+
+//Entity framework puede detectar cual es el PK ID a traves del nombre dle Id y nombre de la clase
 public class Categoria
 {
+    [Key] // Estamos forzando a que utilice la propiedad como clave id
     public Guid CategoriaId {get; set;}
+    [Required]
+    [MaxLength(150)]
     public string Nombre {get; set;}
     public string Decripcion {get; set;}
 
--- projectef/Models/Tarea.cs
+++ projectef/Models/Tarea.cs
@@ -1,18 +1,28 @@
 namespace projectef.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 //Creacion de modelos que se convertiran en tablas de la base de datos
 
 //Clase Tareas y propiedades
 
 // Guid Universally Unique Identifier
+// Cada Tarea va a tener asignada una categoria 
 public class Tarea {
+    [Key]
     public Guid TareaId {get;set;}
+    [ForeignKey("CategoriaId")]
     public Guid CategoriaId {get;set;}
+    [Required]
+    [MaxLength(200)]
     public string Titulo {get;set;}
     public string Descripcion {get;set;}
     public Prioridad PrioridadTarea {get;set;}
     public DateTime FechaCreacion {get; set;}
     public virtual Categoria Categoria {get; set;}
+
+    [NotMapped] //Omite esta propiedad a la base de datos
+    public string Resumen {get;set;}
 }
 
 
